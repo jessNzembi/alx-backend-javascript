8b97fe3c2b270541e69b0e3f5194708c471b7db8
Jess <jessgatura@gmail.com>
2024-02-07 12:05:00 +0300

    ..

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,14 @@
+// function getResponseFromAPI() {
+// 	return new Promise((resolve, reject) => {
+// 		resolve()
+// 		reject()
+// 	});
+// }
+// export default getResponseFromAPI;
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		resolve('success')
-		reject('error')
-	});
+  return new Promise((resolve) => {
+    resolve();
+  });
 }
+
 export default getResponseFromAPI;
